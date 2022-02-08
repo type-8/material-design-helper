0eb5b0d3a0367cb8c0df035d3c8b94a64462189d
type-8 <type.prod.x08@gmail.com>
2022-02-08 23:29:06 +0900

    add: material ripple components

---
+++ src/app/material/ripple/ripple.tsx
@@ -0,0 +1,71 @@
+import { MDCRipple } from '@material/ripple';
+import { createEffect } from 'solid-js';
+import { Directive } from '../../directive';
+
+export interface MdRippleProps {
+  disabled?: boolean;
+  theme?: string;
+  ripple?: (ripple: MDCRipple) => void;
+}
+
+const mdRipple: Directive<MdRippleProps | boolean> = (element, accessor) => {
+  const props = accessor();
+
+  const baseClassName = 'mdc-ripple-surface';
+
+  element.classList.add(baseClassName);
+  const ripple = new MDCRipple(element);
+
+  if (props !== true && props) {
+    props.ripple?.(ripple); // rippleのインスタンスを共有する
+  }
+
+
+  let prevTheme: string | undefined;
+
+
+  /**
+   * @description Rippleを有効にした際に、`element`変数へスタイルを追加・削除する
+   */
+  const activate = (theme?: string) => {
+    ripple.activate();
+
+    const classList = element.classList;
+
+    if (prevTheme) {
+      classList.remove(baseClassName + '--' + prevTheme);
+    }
+
+    if (theme) {
+      classList.add(baseClassName + '--' + theme);
+    }
+
+    prevTheme = theme;
+  };
+
+
+  // `props`に変更があった場合、それに合わせスタイルを変更する
+  createEffect(() => {
+    const props = accessor();
+
+    switch (props) {
+      case true: {
+        activate();
+        break;
+      }
+
+      case false: {
+        ripple.deactivate();
+        break;
+      }
+
+      default: {
+        props.disabled
+          ? ripple.deactivate()
+          : activate(props.theme)
+      }
+    }
+  });
+}
+export default mdRipple;
+
