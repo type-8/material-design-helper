fffe34a5213bf7cabbebd81e939518d421c9c4db
type-8 <type.prod.x08@gmail.com>
2022-02-08 23:25:33 +0900

    add: root footer component

---
+++ src/app/Footer.tsx
@@ -0,0 +1,12 @@
+import MdButton from './material/Button';
+import type { Component } from 'solid-js';
+
+
+const Footer: Component = () => {
+  return (
+    <footer>
+    </footer>
+  );
+};
+
+export default Footer;
