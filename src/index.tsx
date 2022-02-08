82ba983aeec6fd1ce542df3ac8f427f5fefbbbac
type-8 <type.prod.x08@gmail.com>
2022-02-08 23:26:43 +0900

    update: import scss file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { render } from 'solid-js/web';
+import './_index.scss';
+import App from './app/App';
 
-import './index.css';
-import App from './App';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
