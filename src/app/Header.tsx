73263adb6dfa13fd70e7342a9e6c3d7d7241056a
type-8 <type.prod.x08@gmail.com>
2022-02-17 11:08:43 +0900

    update: structure

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,76 +1,58 @@
 import { Link, useLocation } from 'solid-app-router';
+import styles from './Header.module.scss';
+import MdButton from './material/Button';
+import { RoutePath } from './utils';
 import {
-  batch,
   Component,
   createEffect,
   createSelector,
   createSignal,
-  For,
   Index,
-  on
   } from 'solid-js';
-import styles from './Header.module.scss';
-import MdButton from './material/Button';
-import MdTab from './material/tabs/Tab';
-import MdTabBar from './material/tabs/TabBar';
-import type { MDCTabBar } from '@material/tab-bar';
 
 
 const Header: Component = () => {
-  // const [tabBar, setTabBar] = createSignal<MDCTabBar>();
+  const routePath = new RoutePath(
+    ['/color-tools', '/functions'],
+    ['Color Tools', 'Functions']
+  );
 
-  const routePaths = ['/color-tools', '/functions'] as const;
-  const routePathLabels = ['Color Tools', 'Functions'] as const;
-  const routesPathMatchTokens = routePaths.map(path => new RegExp(path));
-
-  const [isActiveRoutePathIndex, setIsActiveRoutePathIndex] = createSignal<number>(null!);
-  
-  const test = createSelector(isActiveRoutePathIndex);
-  // const routerLinkButtonTheme = (index: number) => isActiveRoutePathIndex() === index ? 'primary' : '';
+  const [_isActiveRoutePathIndex, setIsActiveRoutePathIndex] = createSignal<number>(null!);
+  const isActiveRoutePathIndex = createSelector(_isActiveRoutePathIndex);
 
 
   const location = useLocation();
 
   createEffect(() => {
     const pathname = location.pathname;
-    
-    const tokensLen = routePaths.length;
-    for (let i = 0; i < tokensLen; i++) {
-      if (routesPathMatchTokens[i].test(pathname)) {
+
+    const routeLen = routePath.length;
+    const routeRegExps = routePath.regExps;
+
+    for (let i = 0; i < routeLen; i++) {
+      if (routeRegExps[i].test(pathname)) {
         console.log(i);
         setIsActiveRoutePathIndex(i);
         return;
       }
     }
-  });  
-
-  // createEffect(() => {
-  //   const tabBarRef = tabBar();
-  //   if (!tabBarRef) return;
-
-  //   const pathname = location.pathname;
-    
-  //   const tokensLen = routeMatchTokens.length;
-  //   for (let i = 0; i < tokensLen; i++) {
-  //     if (routeMatchTokens[i].test(pathname)) {
-  //       tabBarRef.activateTab(i);
-  //       return;
-  //     }
-  //   }
-  // });
+  });
 
 
+  // <body>の`class`を切り替えることで、テーマを切り替える
   const toggleTheme = () => document.body.classList.toggle('dark-theme');
 
 
   return (
-    <header class={styles['header']}>
+    <header class={styles['host']}>
       <h1 class='mdc-typography--headline6' className={styles['h1']}>Material Design Helper</h1>
 
       <div>
-        <Index each={routePaths}>{(path, index) => (
-          <Link class={`${styles['link']}`} href={path()}>
-            <MdButton class={styles['button']} classList={{'mdc-primary': test(index)}}>{ routePathLabels[index] }</MdButton>
+        <Index each={routePath.names}>{(pathname, index) => (
+          <Link class={`${styles['link']}`} href={pathname()}>
+            <MdButton class={styles['button']} classList={{'mdc-accent': isActiveRoutePathIndex(index)}}>
+              { routePath.labels[index] }
+            </MdButton>
           </Link>
         )}</Index>
 
@@ -84,24 +66,6 @@ const Header: Component = () => {
           </MdButton>
         </a>
       </div>
-      {/* <section class={styles['secondary']}>
-        <MdTabBar className={styles['tab-bar']} tabBar={setTabBar} >
-          <Link class={styles['link']} href="/color-palette">
-            <MdTab>Color Palette</MdTab>
-          </Link>
-          <Link class={styles['link']} href="/approximate-color">
-            <MdTab>Approximate Color</MdTab>
-          </Link>
-        </MdTabBar>
-
-        <div class={styles['bottom-divider-spacer']}>
-          <MdButton class={styles['icon-button']} variant="icon" onClick={toggleTheme}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" aria-hidden="true"><path d="M12,4.81L12,19c-3.31,0-6-2.63-6-5.87c0-1.56,0.62-3.03,1.75-4.14L12,4.81 M12,2L6.35,7.56l0,0C4.9,8.99,4,10.96,4,13.13 C4,17.48,7.58,21,12,21c4.42,0,8-3.52,8-7.87c0-2.17-0.9-4.14-2.35-5.57l0,0L12,2z"/></svg>
-          </MdButton>
-        </div>
-      </section>
-
-      <div class={styles['bottom-divider']}></div> */}
     </header>
   );
 };
