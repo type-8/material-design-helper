5a2ccbf08fedeb94714570e907907cb8756e8154
type-8 <type.prod.x08@gmail.com>
2022-03-29 04:24:56 +0900

    update: config dialog

--- src/app/views/ColorTools/views/Palette/Palette.tsx
+++ src/app/views/ColorTools/views/Palette/Palette.tsx
@@ -1,168 +1,173 @@
 import {
   Component,
+  createEffect,
   createSignal,
-  Index,
-  JSX,
-  Show
+  from,
+  observable,
+  onCleanup,
+  Show,
+  untrack
   } from 'solid-js';
+import { copyText } from '../../../../utils';
+import baseStyles from '../../ColorTools.module.scss';
+import { ColorViewerContextProps, useColorViewer } from '../../ColorViewer';
+import { ConfigContextProp, getConfigFromLocalStorage, saveConfigFromLocalStorage, useConfig } from '../../Config';
+import CopiedSnackbar, { OpenCopiedSnackbarRef } from '../../snackbar/Copied';
+import { COLOR_ELEMENT_CACHE } from './cache';
+import { createPaletteElement, SelectColor } from './element';
+import styles from './Palette.module.scss';
 import {
+  MaterialPaletteKey,
   MATERIAL_DARKER_PALETTE,
   MATERIAL_DEFAULT_PALETTE,
   MATERIAL_LIGHTER_PALETTE,
-  MATERIAL_PALETTE_ACCENT_SHADES,
-  MATERIAL_PALETTE_LABELS,
-  MATERIAL_PALETTE_SHADES,
-  MATERIAL_PALETTE_SIMPLE_SHADES,
-  MaterialPalette,
-  MaterialPaletteShades
   } from '../../../../material/palette';
-import { copyText } from '../../../../utils';
-import { PALETTE_ELEMENT_CATCH } from './cache';
-import styles from './Palette.module.scss';
 
 
-
-
-interface Config {
-  default: boolean;
-  lighter: boolean;
-  darker: boolean;
-
-  animation: boolean;
+type Config = {
+  [key in
+    'default' |
+    'lighter' |
+    'darker' |
+    'shade' |
+    'copy'
+  ]: ConfigContextProp;
+  // animation: boolean;
   // highlight: boolean;
+};
 
-  shouldCopy: boolean;
-  
-}
-
-type SelectColor = (color: string, index: number, event: Event) => void;
-
+const CONFIG_LOCAL_STORAGE_KEY = 'color-tools/palette/config';
 
 const ColorPalette: Component = () => {
-
-  const [config, setConfig] =
-    createSignal(
-      getConfigFromLocalStorage() || {
-        default: true,
-        lighter: true,
-        darker: true,
-        animation: false,
-        shouldCopy: true,
+  const [config, setConfig] = useConfig<Config>();
+  setConfig(
+    getConfigFromLocalStorage<Config>(CONFIG_LOCAL_STORAGE_KEY) || {
+      default: {
+        state: true,
+        label: 'Default'
+      },
+      lighter: {
+        state: true,
+        label: 'Lighter'
+      },
+      darker: {
+        state: true,
+        label: 'Darker'
+      },
+      shade: {
+        state: false,
+        label: 'Show Shade'
+      },
+      copy: {
+        state: true,
+        label: 'Use Copy'
       }
-    );
-
-
-  let prevColorEl: Element | null;
-  const activeColorClassName = styles['active-color'];
-
-  const selectColor: SelectColor = (color, index, event) => {
-    if (prevColorEl) {
-      prevColorEl.classList.remove(activeColorClassName);
     }
+  );
 
-    const colorEl = event.target as Element;
-    if (prevColorEl === colorEl) {
-      prevColorEl = null;
+
+  const openCopiedSnackbar: OpenCopiedSnackbarRef = { ref: null! };
+
+  const selectedColorSignal = useColorViewer.selected()[0];
+
+
+  // クラスを変数に格納（省略用）
+  const activeColorClassName = styles['activated-color'];
+  const colorElCache = COLOR_ELEMENT_CACHE;
+
+  const selectColor: SelectColor = (key, label, shade, color, event) => {    
+    const colorSignal = selectedColorSignal();
+
+    if (colorSignal) {
+      const prevColorEl = colorElCache.get(colorSignal);
+      if (prevColorEl)
+        prevColorEl.classList.remove(activeColorClassName);
+  
+      const colorEl = event.target as Element;
+  
+      if (prevColorEl === colorEl) {
+        colorElCache.set(colorSignal, null);
+        colorSignal[1](null); // CSSスタイルの削除はEffectのObserverで監視済み
+  
+      } else {
+        colorElCache.set(colorSignal, colorEl);
+
+        // CSSスタイルの追加はEffectのObserverで監視済み
+        colorSignal[1]({
+          order: createThemeKeyOrder(key),
+          label, shade,
+          value: {
+            default: MATERIAL_DEFAULT_PALETTE[label][shade]!,
+            lighter: MATERIAL_LIGHTER_PALETTE[label][shade]!,
+            darker:   MATERIAL_DARKER_PALETTE[label][shade]!,
+          }
+        });
+      }
 
     } else {
-      copyText(color);
-      colorEl.classList.add(activeColorClassName);
-      prevColorEl = colorEl;
+      // クリップボードにコピー
+      openCopiedSnackbar.ref(color, event);
     }
   }
 
+
+  // 選択中のパレットが切り替わったとき、または、選択中の色が変更された場合、対応する色の要素を変更する
+  createEffect(() => {
+    const colorSignal = selectedColorSignal();
+
+    if (colorSignal) {
+      // 切り替え時に保存されるデータから色のDOM要素を検索し、それをアクティブ状態の見た目にする
+      colorElCache.forEach((el, signal) => {
+        if (el) {
+          signal === colorSignal
+            ? el.classList.add(activeColorClassName)
+            : el.classList.remove(activeColorClassName)
+        }
+      });
+
+      // 選択中のパレットの色が削除される可能性があるので、変更を監視して対応させる
+      // (なぜか、unsubscribeをしなくても問題なく動作する（しているように見える）)
+      observable(colorSignal[0]).subscribe(color => {
+        const colorEl = colorElCache.get(colorSignal);
+        if (colorEl) {
+          color === null
+            ? colorEl?.classList.remove(activeColorClassName)
+            : colorEl.classList.add(activeColorClassName);
+        }
+      });
+
+    } else {
+      // パレットがどれも選択されていない場合、すべてのスタイルを削除する
+      colorElCache.forEach((el) => el?.classList.remove(activeColorClassName));
+    }
+  });
+
+
+  onCleanup(() => {
+    saveConfigFromLocalStorage(CONFIG_LOCAL_STORAGE_KEY, config());
+  });
+
+
   return (
-    <section class={styles.host}>
-      <Show when={config().default}>{ createPaletteElement(MATERIAL_DEFAULT_PALETTE, selectColor) }</Show>
-      {/* <div>{ createPaletteElement(MATERIAL_LIGHTER_PALETTE) }</div> */}
-      {/* <div>{ createPaletteElement(MATERIAL_DARKER_PALETTE) }</div> */}
-    </section>
+    <div class={`${styles.host} ${baseStyles['route-host']}`} classList={{ [styles['shown-shade']]: config().shade.state }}>
+      <Show when={config().default.state}>{ createPaletteElement('default', MATERIAL_DEFAULT_PALETTE, selectColor) }</Show>
+      <Show when={config().lighter.state}>{ createPaletteElement('lighter', MATERIAL_LIGHTER_PALETTE, selectColor) }</Show>
+      <Show when={config().darker.state}>{  createPaletteElement('darker',  MATERIAL_DARKER_PALETTE,  selectColor) }</Show>
+
+      <CopiedSnackbar open={openCopiedSnackbar} />
+    </div>
   );
 };
 
 export default ColorPalette;
 
 
-// red => Red
-// deepOrange => Deep Orange
-function capitalizeLabel(label: string): string {
-  return label.replace(/[A-Z]/, (substring) => ' ' + substring);
-}
-
-
-const SHADE_ELEMENTS = <>
-  <div class={styles['shade']}></div>
-  <Index each={MATERIAL_PALETTE_SHADES}>{(shade) => <div class={styles['shade']}>{shade}</div>}</Index>
-</>;
-
-
-type CreatePaletteElement = (palette: MaterialPalette, selectColor: SelectColor) => JSX.Element;
-/**
- * @description パレットを表示するDOMを生成する。一度生成したDOMは保存され、`ColorTools`Componentが破棄されるまで保持される。
- * @param palette
- * @param selectColor 子要素がクリックされたときに発火する
- */
-const createPaletteElement: CreatePaletteElement = (palette, selectColor) => {
-  const cache = PALETTE_ELEMENT_CATCH;
-  let paletteElement = cache.get(palette);
-
-  if (!paletteElement) {
-    paletteElement = (
-      <div class={styles['palette']}>  
-        { SHADE_ELEMENTS }
-        <Index each={MATERIAL_PALETTE_LABELS}>{(_label) => {
-          const label = _label();
-          const colorRef = palette[label];
-          const createPaletteColor = createColorElement.bind(null, colorRef, selectColor);
-
-          const accentColorEls = colorRef.A100
-            ? <Index each={MATERIAL_PALETTE_ACCENT_SHADES}>{createPaletteColor}</Index>
-            : <div class={styles['empty-color']}></div>;
-
-          return (
-            <>
-              <div class={styles['label']}>{ capitalizeLabel(label) }</div>
-              <Index each={MATERIAL_PALETTE_SIMPLE_SHADES}>{createPaletteColor}</Index>
-              { accentColorEls }
-            </>
-          )
-        }}</Index>
-      </div>
-    );
-    cache.set(palette, paletteElement);
-  }
-
-  return paletteElement;
-}
-
-
-type CreateColorElement = (colorRef: MaterialPalette['red'], selectColor: SelectColor, shade: () => MaterialPaletteShades[number], index: number) => JSX.Element;
-/** @description `createPaletteElement`内部で使用される関数。色を表すDOMを生成する。 */
-const createColorElement: CreateColorElement = (colorRef, selectColor, _shade, index) => {
-  const shade = _shade();
-  const { color, contrast } = colorRef[shade]!;
-
+function createThemeKeyOrder(key: MaterialPaletteKey): ColorViewerContextProps['order'] {
   return (
-    <div onClick={(event) => selectColor(color, index, event)}
-      class={/*@once*/`${styles['color']} ${styles[contrast]}`}
-      style={/*@once*/`background-color:${color}`}
-    >
-      {shade}
-    </div>
+    key === 'lighter'
+      ? ['lighter', 'darker', 'default']
+      : key === 'darker'
+        ? ['darker', 'default', 'lighter']
+        : ['default', 'lighter', 'darker']
   )
-}
-
-
-const CONFIG_LOCAL_STORAGE_KEY = 'color-tools/palette/config';
-
-function getConfigFromLocalStorage(): Config | null {
-  const displayConfig = localStorage.getItem(CONFIG_LOCAL_STORAGE_KEY);
-
-  return displayConfig
-    ? JSON.parse(displayConfig)
-    : null;
-}
-
-function setConfigToLocalStorage(config: Config): void {
-  localStorage.setItem(CONFIG_LOCAL_STORAGE_KEY, JSON.stringify(config));
 }
