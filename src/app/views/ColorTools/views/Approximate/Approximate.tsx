f59936f23ecffcaea6e9218d49ea560e4621fe2e
type-8 <type.prod.x08@gmail.com>
2022-03-29 04:47:21 +0900

    update: config dialog

--- src/app/views/ColorTools/views/Approximate/Approximate.tsx
+++ src/app/views/ColorTools/views/Approximate/Approximate.tsx
@@ -1,9 +1,34 @@
-import type { Component } from 'solid-js';
+import { Component, onCleanup } from 'solid-js';
+import baseStyles from '../../ColorTools.module.scss';
+import { getConfigFromLocalStorage, saveConfigFromLocalStorage, useConfig } from '../../Config';
 
+const CONFIG_LOCAL_STORAGE_KEY = 'color-tools/approximate/config';
 
 const ApproximateColor: Component = () => {
-  return (<>
-  </>);
+  const [config, setConfig] = useConfig();
+  setConfig(
+    getConfigFromLocalStorage(CONFIG_LOCAL_STORAGE_KEY) || {
+      shade: {
+        state: false,
+        label: 'Show Shade'
+      },
+      copy: {
+        state: true,
+        label: 'Use Copy'
+      }
+    }
+  );
+
+
+  onCleanup(() => {
+    saveConfigFromLocalStorage(CONFIG_LOCAL_STORAGE_KEY, config());
+  });
+
+
+  return (
+    <div class={`${baseStyles['route-host']}`}>
+    </div>
+  );
 };
 
 export default ApproximateColor;
