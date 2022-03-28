ad6d52b2631f1cfb57580a3e2190bf3994a988c5
type-8 <type.prod.x08@gmail.com>
2022-03-29 04:22:03 +0900

    update: added method to check if path match

--- src/app/utils/route-path.ts
+++ src/app/utils/route-path.ts
@@ -1,16 +1,36 @@
 export class RoutePath {
+  readonly names: string[];
   readonly regExps: RegExp[];
   readonly length: number;
 
+
   constructor(
-    public readonly names: string[],
-    public readonly labels: string[]
+    names: string[],
+    public readonly labels: string[],
+    public readonly base?: string
   ) {
+    this.names = base
+      ? (names = names.map(name => '/' + base + '/' + name))
+      : (names = names.map(name => '/' + name));
+
     const length = this.length = names.length;
 
     const regExps = this.regExps = [] as RegExp[];
     for (let i = 0; i < length; i++) {
       regExps.push(new RegExp(names[i]));
+    }
+  }
+
+
+  checkPathnameMatches(pathname: string, onMatch: (index: number) => void): void {
+    const routeLen = this.length;
+    const routeRegExps = this.regExps;
+
+    for (let i = 0; i < routeLen; i++) {
+      if (routeRegExps[i].test(pathname)) {
+        onMatch(i);
+        return;
+      }
     }
   }
 }
