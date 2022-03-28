5c222243008aeb0d84e9919686ef327ff11bc4e5
type-8 <type.prod.x08@gmail.com>
2022-03-29 04:29:23 +0900

    udpate: element cache

--- src/app/views/ColorTools/views/Palette/cache.ts
+++ src/app/views/ColorTools/views/Palette/cache.ts
@@ -1,6 +1,11 @@
+import { MaterialPaletteKey } from '../../../../material/palette';
+import { ColorViewerSignal } from '../../ColorViewer';
 import type { JSX } from 'solid-js';
-import type { MaterialPalette } from "../../../../material/palette";
 
-export type PaletteElementCatch = Map<MaterialPalette, JSX.Element>;
 
-export const PALETTE_ELEMENT_CATCH: PaletteElementCatch = new Map();
+export type PaletteElementCache = Map<MaterialPaletteKey, JSX.Element>;
+export const PALETTE_ELEMENT_CACHE: PaletteElementCache = new Map();
+
+
+export type ColorElementCache = Map<ColorViewerSignal, Element | null>;
+export const COLOR_ELEMENT_CACHE: ColorElementCache = new Map();
