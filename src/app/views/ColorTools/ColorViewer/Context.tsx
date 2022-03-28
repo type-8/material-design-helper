f31ad5ac06cd81a86d35a122c8c864a94f713706
type-8 <type.prod.x08@gmail.com>
2022-03-29 04:23:42 +0900

    add: config context for ColorViewer

---
+++ src/app/views/ColorTools/ColorViewer/Context.tsx
@@ -0,0 +1,37 @@
+import { createContext, Signal, useContext } from 'solid-js';
+import {
+  MaterialColor,
+  MaterialPaletteKey,
+  MaterialPaletteKeys,
+  MaterialPaletteLabels,
+  MaterialPaletteShades
+  } from '../../../material/palette';
+
+
+export type ColorViewerKeys = ['primary', 'secondary'];
+export const COLOR_VIEWER_KEYS: ColorViewerKeys = ['primary', 'secondary'];
+
+
+type PltKeys = MaterialPaletteKeys;
+export interface ColorViewerContextProps {
+  order: [PltKeys[0], PltKeys[1], PltKeys[2]] | [PltKeys[1], PltKeys[2], PltKeys[0]] | [PltKeys[2], PltKeys[0], PltKeys[1]];
+  label: MaterialPaletteLabels[number];
+  shade: MaterialPaletteShades[number];
+  value: {
+    [key in MaterialPaletteKey]: MaterialColor;
+  }
+}
+
+
+export type ColorViewerSignal = Signal<ColorViewerContextProps | null>;
+
+export const PrimaryColorViewerContext = createContext<ColorViewerSignal>();
+export const SecondaryColorViewerContext = createContext<ColorViewerSignal>();
+export const SelectedColorViewerContext = createContext<Signal<ColorViewerSignal | null>>();
+
+
+export const useColorViewer = {
+  primary: () => useContext(PrimaryColorViewerContext)!,
+  secondary: () => useContext(SecondaryColorViewerContext)!,
+  selected: () => useContext(SelectedColorViewerContext)!,
+} as const;
