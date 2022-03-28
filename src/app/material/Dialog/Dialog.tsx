176bd5506bc70f287feb9282b78298310c750c6b
type-8 <type.prod.x08@gmail.com>
2022-03-29 04:15:53 +0900

    add: dialog converted to solid components

---
+++ src/app/material/Dialog/Dialog.tsx
@@ -0,0 +1,73 @@
+import { Component, createEffect, Show } from 'solid-js';
+import { Portal } from 'solid-js/web';
+import { MdDialogCore } from './core';
+import type { MDCDialogAdapter } from '@material/dialog';
+
+
+
+// Dialogを重複して表示させないために用いる
+let displayedDialog: MdDialogCore | null = null;
+
+
+export interface MdDialogProps {
+  opened: boolean;
+  onClose: (opened: false) => void;
+  config?: Partial<MDCDialogAdapter>;
+}
+
+const MdDialog: Component<MdDialogProps> = (props) => {
+  let element: HTMLDivElement | null;
+  let dialog: MdDialogCore | null;
+
+  // Dialogが出現しているときに`props.config`に変更があった場合
+  createEffect(() => {
+    const config = props.config;
+    if (config && dialog) {
+      dialog.mergeAdapter(config);
+    }
+  });
+
+
+  const onClose = () => {
+    props.onClose(false);
+    element = dialog = displayedDialog = null;
+  }
+
+  // `props.opened`が`true`となり、ダイアログを開く処理
+  createEffect(() => {
+    if (props.opened && element) {
+      // 表示済みのDialogがあれば削除
+      if (displayedDialog) displayedDialog.close();
+
+      dialog = displayedDialog = new MdDialogCore(element, props.config);
+      dialog.open();
+      dialog.listen('MDCDialog:closed', onClose);
+    }
+  });
+
+
+  return (
+    <Show when={props.opened}>
+      <Portal>
+        <div ref={element!} class="mdc-dialog">
+          <div class="mdc-dialog__container">
+            <div class="mdc-dialog__surface" role="alertdialog" aria-modal="true">
+              { props.children }
+            </div>
+          </div>
+          <div class="mdc-dialog__scrim"></div>
+        </div>
+      </Portal>
+    </Show>
+  );
+}
+export default MdDialog;
+
+
+
+export const MdDialogStyles = {
+  title: 'mdc-dialog__title',
+  content: 'mdc-dialog__content',
+  actions: 'mdc-dialog__actions',
+  button: 'mdc-dialog__button'
+};
