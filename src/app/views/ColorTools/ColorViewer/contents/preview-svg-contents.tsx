5a35071c10a2f5e43345110e9e572cc31e645633
type-8 <type.prod.x08@gmail.com>
2022-03-29 04:28:56 +0900

    add: array containing svg content

---
+++ src/app/views/ColorTools/ColorViewer/contents/preview-svg-contents.tsx
@@ -0,0 +1,179 @@
+export default [
+  // 1
+  <g>
+    <rect class="cvt-darker-primary" x="0.5" y="0.5" width="363.6" height="24.2"></rect>
+
+    <g class="cvt-default-primary">
+      <rect x="0.5" y="24.2" width="363.6" height="64.6"></rect>
+      <text x="32.2" y="65.2" style="font-size:24px">Text</text>
+    </g>
+
+    <rect class="mdc-theme-fill-secondary" x="0.5" y="267.2" width="363.6" height="379.8"></rect>
+    <rect class="mdc-theme-fill-tertiary" x="0.5" y="89.4" width="363.6" height="242.4"></rect>
+
+    <g class="cvt-default-secondary cvt-default-primary">
+      <circle cx="322.6" cy="331.5" r="25.4"></circle>
+      <line stroke-width="3" fill="none" stroke-miterlimit="10" x1="322.6" y1="318.5" x2="322.6" y2="343.7"></line>
+      <line stroke-width="3" fill="none" stroke-miterlimit="10" x1="335.2" y1="331.1" x2="310" y2="331.1"></line>
+    </g>
+
+    <rect class="mdc-theme-fill-divider" x="229.7" y="418.7" width="118.2" height="2"></rect>
+    <g class="cvt-default-secondary cvt-default-primary">
+      <rect x="229.7" y="418.7" width="70.7" height="2"></rect>
+      <path d="M300.5,415c-2.7,0-4.9,2.2-4.9,4.9c0,2.7,2.2,4.9,4.9,4.9c2.7,0,4.9-2.2,4.9-4.9 C305.5,417.2,303.3,415,300.5,415"></path>
+    </g>
+  </g>,
+
+
+  // 2
+	<g>
+    <rect class="mdc-theme-fill-secondary" x="0.3" y="267.2" width="363.6" height="379.8"></rect>
+
+    <rect class="cvt-default-primary" x="0.3" y="24.8" width="363.6" height="244.4"></rect>
+
+    <g class="cvt-darker-primary">
+      <rect x="0.3" y="0.5" width="363.6" height="88.9"></rect>
+      <text x="32.1" y="57.1" style="font-size:24px">Text</text>
+    </g>
+
+    <g class="cvt-lighter-primary" style="font-size:14px">
+      <rect x="0.3" y="269.2" width="363.6" height="63.3"></rect>
+      <text x="32.1" y="310.5">ITEM 1</text>
+      <text x="156.4" y="310.5">ITEM 2</text>
+      <text x="277.6" y="310.5">ITEM 3</text>
+    </g>
+    <rect class="cvt-default-secondary cvt-default-primary" x="0.3" y="328.5" width="121.2" height="4"></rect>
+
+    <g class="cvt-default-secondary cvt-default-primary">
+      <circle cx="322.4" cy="562.5" r="25.4"></circle>
+      <line stroke-width="3" fill="none" stroke-miterlimit="10" x1="322.4" y1="549.5" x2="322.4" y2="574.7"></line>
+      <line stroke-width="3" fill="none" stroke-miterlimit="10" x1="335" y1="562.1" x2="309.8" y2="562.1"></line>
+    </g>
+	</g>,
+
+
+  // 3
+  <g>
+    <g class="cvt-default-primary">
+      <rect x="0.6" y="0" width="363.6" height="88.9"></rect>
+      <text x="38.2" y="44.6" font-size="24px">Text</text>
+    </g>
+
+		<rect class="mdc-theme-fill-tertiary" x="0.6" y="88.9" width="363.6" height="557.6"></rect>
+
+    <rect class="mdc-theme-fill-card" x="42.4" y="65.2" width="280" height="350"></rect>
+    <path class="cvt-darker-secondary cvt-darker-primary" d="M163.5,224.7l26.3-45.5c-7.7-4.6-16.7-7.2-26.3-7.2c-28.6,0-51.7,23.2-51.7,51.7c0,28.6,23.2,51.7,51.7,51.7 c28.3,0,51.2-22.6,51.7-50.8H163.5z"></path>
+    <path class="cvt-darker-secondary cvt-darker-primary" d="M215.2,223.7c0,0.3,0,0.6,0,1H267L215.2,135l-25.5,44.1C205,188.1,215.2,204.7,215.2,223.7z"></path>
+    <path class="cvt-lighter-secondary cvt-lighter-primary" d="M215.2,224.7c0-0.3,0-0.6,0-1c0-19-10.2-35.6-25.5-44.6l-26.3,45.5H215.2z"></path>
+
+    <rect class="mdc-theme-fill-card" x="42.4" y="454.8" width="280" height="190.5"></rect>
+    <path class="cvt-darker-secondary cvt-darker-primary" d="m164 562c-28.6 0-51.7 23.2-51.7 51.7 0 12.7 4.58 24.3 12.2 33.3h79.1c7.38-8.77 11.9-20 12.1-32.4h-51.6l26.2-45.4c-7.7-4.6-16.7-7.2-26.3-7.2z"/>
+    <path class="cvt-darker-secondary cvt-darker-primary" d="M215.2,613.3c0,0.3,0,0.6,0,1H267l-51.7-89.6l-25.5,44.1C205,577.7,215.2,594.3,215.2,613.3z"/>
+    <path class="cvt-lighter-secondary cvt-lighter-primary" d="M215.2,614.2c0-0.3,0-0.6,0-1c0-19-10.2-35.6-25.5-44.6l-26.3,45.5H215.2z"/>
+
+		<g class="cvt-default-secondary cvt-default-primary">
+			<path d="M304.2,393.4c0,1-0.7,1.8-1.7,1.8h-84.7c-0.9,0-1.6-0.8-1.6-1.8V361c0-1,0.7-1.8,1.6-1.8h84.7 c0.9,0,1.7,0.8,1.7,1.8V393.4z"></path>
+      <text x="240.9" y="382.9" style="font-size:16px;font-weight:500">TEXT</text>
+		</g>
+
+    <g class="cvt-darker-secondary cvt-darker-primary">
+      <circle cx="322.7" cy="562" r="25.4"></circle>
+      <line stroke-width="3" fill="none" stroke-miterlimit="10" x1="322.7" y1="549" x2="322.7" y2="574.2"></line>
+      <line stroke-width="3" fill="none" stroke-miterlimit="10" x1="335.3" y1="561.6" x2="310.1" y2="561.6"></line>
+    </g>
+	</g>,
+
+
+  // 4
+  <g>
+		<rect class="mdc-theme-fill-secondary" x="0.3" y="89.4" width="363.6" height="557.6"></rect>
+		<rect class="cvt-lighter-primary" x="0.3" y="558.1" width="363.6" height="88.9"></rect>
+    <rect class="cvt-default-primary" x="0.3" y="0.5" width="363.6" height="88.9"></rect>
+		<rect class="cvt-darker-primary" x="0.3" y="0.5" width="363.6" height="24.2"></rect>
+
+    <rect fill="black" opacity="0.24" x="0.3" y="0.5" width="363.6" height="646.5"></rect>
+		<rect class="mdc-theme-fill-primary" x="0.3" y="0.5" width="300" height="646.5"></rect>
+
+    <rect class="cvt-default-secondary cvt-default-primary" x="0.3" y="0.5" width="300.2" height="300.2"></rect>
+
+    <path class="cvt-lighter-secondary cvt-lighter-primary" d="M124.5,171l26.3-45.5c-7.7-4.6-16.7-7.2-26.3-7.2c-28.6,0-51.7,23.2-51.7,51.7c0,28.6,23.2,51.7,51.7,51.7 c28.3,0,51.2-22.6,51.7-50.8H124.5z"></path>
+    <path class="cvt-lighter-secondary cvt-lighter-primary" d="M176.2,170.1c0,0.3,0,0.6,0,1H228l-51.7-89.6l-25.5,44.1C166,134.5,176.2,151.1,176.2,170.1z"></path>
+    <path class="cvt-darker-secondary cvt-darker-primary" d="M176.2,171c0-0.3,0-0.6,0-1c0-19-10.2-35.6-25.5-44.6L124.5,171H176.2z"></path>
+
+    <path class="cvt-darker-secondary cvt-darker-primary" d="M263.3,349.1h-20c-3.9,0-7-3.1-7-7l0,0c0-3.9,3.1-7,7-7h20c3.9,0,7,3.1,7,7l0,0 C270.3,345.9,267.1,349.1,263.3,349.1z"></path>
+		<circle class="cvt-lighter-secondary cvt-lighter-primary" cx="263.3" cy="342.1" r="10"></circle>
+	</g>,
+
+
+  // 5
+  <g>
+    <rect class="cvt-default-primary" x="0.5" y="0.5" width="363.6" height="646.5"></rect>
+    <rect class="cvt-lighter-primary" x="0.5" y="558.1" width="363.6" height="88.9"></rect>
+    <rect class="cvt-darker-primary" x="0.5" y="0.5" width="363.6" height="24.2"></rect>
+
+    <rect class="mdc-theme-fill-card" x="42.3" y="65.8" width="280" height="350"></rect>
+    <rect class="mdc-theme-fill-card" x="343.8" y="65.8" width="20" height="350"></rect>
+    <rect class="mdc-theme-fill-card" x="0.5" y="65.8" width="20" height="350"></rect>
+
+    <path class="cvt-default-secondary cvt-default-primary" d="M125.3,267l57-98.7l57,98.7c25.3,43.9-6.3,98.7-57,98.7h0C131.6,365.6,100,310.8,125.3,267z"></path>
+
+    <g class="cvt-darker-secondary cvt-darker-primary">
+      <rect x="42.3" y="65.8" width="280" height="50"></rect>
+      <text x="62.3" y="99.2" style="font-size:24px">Text</text>
+    </g>
+    <rect class="cvt-darker-secondary cvt-darker-primary" x="343.5" y="65.8" width="20.6" height="50"></rect>
+    <rect class="cvt-darker-secondary cvt-darker-primary" x="0.2" y="65.8" width="20.6" height="50"></rect>
+
+    <text style="font-size:15px">
+      <tspan x="42.3" y="455.3">Lorem ipsum dolor sit amet, consectetur</tspan>
+      <tspan x="42.3" y="475.3">adipiscing elit. Donec viverra eget nulla</tspan>
+      <tspan x="42.3" y="495.3">vitae ornare.</tspan>
+    </text>
+
+    <g class="cvt-darker-secondary cvt-darker-primary">
+      <circle cx="303.3" cy="559.4" r="25.4"></circle>
+      <line stroke-width="3" fill="none" stroke-miterlimit="10" x1="303.3" y1="546.4" x2="303.3" y2="571.6"></line>
+      <line stroke-width="3" fill="none" stroke-miterlimit="10" x1="315.9" y1="559" x2="290.8" y2="559"></line>
+    </g>
+  </g>,
+
+
+  // 6
+  <g>
+    <rect class="mdc-theme-fill-tertiary" x="0.3" width="363.3" height="645.8"></rect>
+    
+    <g class="cvt-default-primary">
+      <rect x="0.3" width="363.3" height="99.9"></rect>
+      <text x="40.6754" y="40.2647" style="font-size:24px">Text</text>
+    </g>
+    <rect class="cvt-lighter-primary" x="0.3" y="557" width="363.3" height="88.8"></rect>
+
+    <rect class="mdc-theme-fill-card" x="40.7" y="62.6" width="282.6" height="454.1"></rect>
+
+    <rect class="mdc-theme-fill-divider" x="50.8" y="86.8" width="10" height="10"></rect>
+    <text x="73" y="96.8">One</text>
+
+    <g class="cvt-default-secondary cvt-default-primary">
+      <rect x="40.7" y="153.4" width="282.6" height="90.8"></rect>
+      <text x="73.0329" y="187.6207">Two</text>
+    </g>
+    <rect class="cvt-darker-secondary cvt-darker-primary" x="50.8" y="177.6" width="10" height="10"></rect>
+
+    <line class="mdc-theme-fill-divider" x1="40.7" y1="335" x2="323.2" y2="335"></line>
+    <rect class="mdc-theme-fill-divider" x="50.8" y="268.4" width="10" height="10"></rect>
+    <text x="73.0329" y="278.443">Three</text>
+
+    <line class="mdc-theme-fill-divider" x1="40.7" y1="425.9" x2="323.2" y2="425.9"></line>
+    <rect class="mdc-theme-fill-divider" x="50.8" y="359.3" width="10" height="10"></rect>
+    <text x="73.0329" y="369.2637">Four</text>
+
+    <rect class="mdc-theme-fill-divider" x="50.8" y="450.1" width="10" height="10"></rect>
+    <text x="73.0329" y="460.088">Five</text>
+
+    <g class="cvt-darker-secondary cvt-darker-primary">
+      <circle cx="303.3" cy="559.4" r="25.4"></circle>
+      <line stroke-width="3" fill="none" stroke-miterlimit="10" x1="303.3" y1="546.4" x2="303.3" y2="571.6"></line>
+      <line stroke-width="3" fill="none" stroke-miterlimit="10" x1="315.9" y1="559" x2="290.8" y2="559"></line>
+    </g>
+  </g>
+];
