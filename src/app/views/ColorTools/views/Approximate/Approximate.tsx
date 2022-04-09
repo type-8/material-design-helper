6261521a9c10c88c6e18ad0550950ea3f4a4f125
junpei10 <miurajunpeis2@gmail.com>
2022-04-10 04:48:13 +0900

    update: Apporoximate page

--- src/app/views/ColorTools/views/Approximate/Approximate.tsx
+++ src/app/views/ColorTools/views/Approximate/Approximate.tsx
@@ -1,6 +1,8 @@
 import { Component, onCleanup } from 'solid-js';
 import baseStyles from '../../ColorTools.module.scss';
 import { CONFIG_VERSION, ConfigContextProps, getConfigFromLocalStorage, useConfig } from '../../Config';
+import styles from './Approximate.module.scss';
+
 
 const CONFIG_LOCAL_STORAGE_KEY = 'color-tools/approximate/config';
 
@@ -21,14 +23,30 @@ const INITIAL_CONFIG: ConfigContextProps = {
   }
 };
 
+const SUPPORTED_WORKER = !!window.Worker;
 
 const ApproximateColor: Component = () => {
   const [config, setConfig] = useConfig();
   setConfig(getConfigFromLocalStorage(CONFIG_LOCAL_STORAGE_KEY, INITIAL_CONFIG));
 
 
+  const calculateColor = (event: SubmitEvent) => {
+    event.preventDefault();
+
+    if (SUPPORTED_WORKER) {
+      const worker = new window.Worker('./worker.ts');
+
+    } else {
+
+    }
+
+  };
+
   return (
     <div class={`${baseStyles['route-host']}`}>
+      <form class={styles['input-form']} onSubmit={calculateColor}>
+        <input class={styles.input}></input>
+      </form>
     </div>
   );
 };
